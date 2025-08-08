687e03d0fd35a2746f097d7cd949cabfa861ca61
Starving_Orange <kyungjjoo@gmail.com>
2025-08-09 04:09:25 +0900

    feature(마이 페이지): 요청서 목록 api 연결 완료

--- src/pages/Home/UserMainPage.tsx
+++ src/pages/Home/UserMainPage.tsx
@@ -1,4 +1,4 @@
-timport 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 
 import Banner from '@/components/Banner';
